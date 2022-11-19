8b7727f0eecde9f7c5a397a260ff560f4c4dfed1
Maahdima <68284989+Maahdima@users.noreply.github.com>
2022-11-19 12:33:53 +0330

    test

--- firstapp.js
+++ firstapp.js
@@ -1,6 +1,14 @@
-const express = require("express");
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
+
 const app = express();
 const port = process.env.PORT || 9000;
+var db = require("./database.js");
+
 app.get("/", (req, res) => {
   res.type("text/plain");
   res.send("Server Expresso ☕");
@@ -9,6 +17,21 @@ app.get("/about", (req, res) => {
   res.type("text/plain");
   res.send("Server Expresso ☕ About");
 });
+
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "../client/build")));
+app.use("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/testAPI", testAPIRouter);
+app.use("/login", loginRouter);
 app.use((req, res) => {
   res.type("text/plain");
   res.status(404);
